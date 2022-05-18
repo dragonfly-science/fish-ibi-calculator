8ad1c4907d32e3d3b6e98cd56e756d836f307332
yvanrichard <yvan@dragonfly.co.nz>
2022-05-18 15:08:14 +1200

    Strata with nospec are now included

--- app/fishr-functions.R
+++ app/fishr-functions.R
@@ -43,6 +43,11 @@ prep.site.metrics <- function(.data, species.ibi.metrics = species_ibi_metrics) 
   .data <- .data[rowid(Stratum, SpeciesCode) == 1]
   
   sim <- as.data.table(species.ibi.metrics)
+  nsp <- sim[1]
+  nsp[, spcode := 'nospec']
+  for (v in setdiff(names(nsp), 'spcode')) nsp[, eval(v) := 0]
+  sim <- rbind(sim, nsp)
+
   m <- merge(.data, sim, by.x = 'SpeciesCode', by.y = 'spcode')
 
   ms <- m[
@@ -50,7 +55,7 @@ prep.site.metrics <- function(.data, species.ibi.metrics = species_ibi_metrics) 
       SiteID            = first(SiteID),
       Altitude          = first(Altitude),
       Penet             = first(Penetration),
-      total_sp_richness = .N,
+      total_sp_richness = .SD[SpeciesCode != 'nospec', .N],
       metric1           = sum(native, na.rm = T),
       metric2           = sum(benthic_riffle, na.rm = T),
       metric3           = sum(benthic_pool, na.rm = T),
@@ -58,9 +63,9 @@ prep.site.metrics <- function(.data, species.ibi.metrics = species_ibi_metrics) 
       metric5           = sum(intolerant, na.rm = T),
       number_non_native = sum(non_native, na.rm = T))
   , Stratum][
-   , metric6 := metric1 / (number_non_native + metric1)
+   , metric6 := fifelse(total_sp_richness > 0, metric1 / (number_non_native + metric1), 0)
   ]
-
+  
   ms
  
 }
@@ -123,6 +128,8 @@ add.fish.metrics <- function(.data, q1e = qr.1.elev, q2e = qr.2.elev, q3e = qr.3
     metric4_rating_pene = qr.score(x = Penet, y = metric4, qr = q4p),
     metric5_rating_pene = qr.score(x = Penet, y = metric5, qr = q5p))
   , by = 1:nrow(.data)]
+  for (v in grep('_rating_', names(d1), val = T, fixed = T))
+    d1[total_sp_richness == 0, eval(v) := NA_real_]
   d1
 }
 
@@ -132,7 +139,9 @@ add.fish.metrics <- function(.data, q1e = qr.1.elev, q2e = qr.2.elev, q3e = qr.3
 #'
 #' @export
 add.fish.metric6 <- function(.data) {
-  .data[, metric6_rating := fcase(
+  .data[
+  , metric6_rating := fcase(
+    total_sp_richness == 0, NA_real_,
     # these values are coming from table 3 on page 421
     metric6 >  0.67, 5,
     metric6 >= 0.33, 3,
@@ -147,18 +156,19 @@ add.fish.metric6 <- function(.data) {
 #'
 #' @export
 add.fish.ibi <- function(.data){
-  .data[, IBIscore :=
-             metric1_rating_elev +
-             metric2_rating_elev +
-             metric3_rating_elev +
-             metric4_rating_elev +
-             metric5_rating_elev +
-             metric1_rating_pene +
-             metric2_rating_pene +
-             metric3_rating_pene +
-             metric4_rating_pene +
-             metric5_rating_pene +
-             metric6_rating*2]
+  .data[
+      , IBIscore :=
+          metric1_rating_elev +
+          metric2_rating_elev +
+          metric3_rating_elev +
+          metric4_rating_elev +
+          metric5_rating_elev +
+          metric1_rating_pene +
+          metric2_rating_pene +
+          metric3_rating_pene +
+          metric4_rating_pene +
+          metric5_rating_pene +
+          metric6_rating*2]
 }
 
 #' Cut IBI score
@@ -168,17 +178,21 @@ add.fish.ibi <- function(.data){
 #'
 #' @export
 cut.fish.ibi <- function(.data) {
-  .data[, IBIscoreCut := cut(IBIscore, breaks = c(0, 20, 40, 60),
-                            labels = c("Low quality",
-                                       "Medium quality",
-                                       "High quality"))]
+  labs <- c("Low quality", "Medium quality", "High quality")
+  .data[total_sp_richness > 0
+      , IBIscoreCut := cut(IBIscore, breaks = c(0, 20, 40, 60), labels = labs)]
+  .data[, IBIscoreCut := factor(as.character(IBIscoreCut), levels = c('No IBI species', labs))]
+  .data[total_sp_richness == 0, IBIscoreCut := 'No IBI species']
+  .data
 }
 
 nps <- function(.data) {
-  .data[, NPSscore := fcase(
-      IBIscore >= 34, "A",
-      IBIscore >= 28, "B",
-      IBIscore >= 18, "C",
-      IBIscore <  18, "D"
+  .data[
+  , NPSscore := fcase(
+    total_sp_richness == 0, "No species",
+    IBIscore >= 34, "A",
+    IBIscore >= 28, "B",
+    IBIscore >= 18, "C",
+    IBIscore <  18, "D"
   )]
 }
